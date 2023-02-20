1912a2e317ebbb085cf93b75d70cf7a629778181
Nikita Lendiak <lendiak93@gmail.com>
2023-02-20 15:06:17 +0000

    add tests for /api/reviews endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -36,6 +36,48 @@ describe("app", () => {
       });
     });
   });
+
+  describe("/api/reviews", () => {
+    describe("GET", () => {
+      describe("Successful Responses", () => {
+        test("200 - responds with an array of reviews objects including comment_count in descending order by created_at", () => {
+          return request(app)
+            .get("/api/reviews")
+            .expect(200)
+            .then(({ body }) => {
+              const { reviews } = body;
+
+              reviews.forEach((review) => {
+                expect(review).toHaveProperty("owner", expect.any(String));
+                expect(review).toHaveProperty("title", expect.any(String));
+                expect(review).toHaveProperty("review_id", expect.any(Number));
+                expect(review).toHaveProperty("category", expect.any(String));
+                expect(review).toHaveProperty(
+                  "review_img_url",
+                  expect.any(String)
+                );
+                expect(review).toHaveProperty("created_at", expect.any(String));
+                expect(review).toHaveProperty("votes", expect.any(Number));
+                expect(review).toHaveProperty("designer", expect.any(String));
+                expect(review).toHaveProperty(
+                  "comment_count",
+                  expect.any(Number)
+                );
+              });
+              expect(reviews).toHaveLength(13);
+
+              const reviewsCommentsTotalCount = eval(
+                reviews.map((review) => review.comment_count).join("+")
+              );
+              expect(reviewsCommentsTotalCount).toBe(6);
+
+              expect(reviews).toBeSorted("created_at", { descending: true });
+            });
+        });
+      });
+    });
+  });
+
   describe("/api/non-existent-endpoint", () => {
     describe("GET", () => {
       test("404 - should response with 'Not Found' message", () => {
