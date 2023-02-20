94c8ce9e114dd3cb201740486a79b5c8da4f3a44
Nikita Lendiak <lendiak93@gmail.com>
2023-02-20 13:44:01 +0000

    add error handlers

---
+++ controllers/error-handlers.controller.js
@@ -0,0 +1,28 @@
+const notFoundErrorsHandler = (req, res, next) => {
+  res.status(404).send({ message: "Not Found" });
+};
+
+const customErrorsHandler = (err, req, res, next) => {
+  if (err.statusCode && err.message) {
+    res.status(err.statusCode).send({ message: err.message });
+  } else next(err);
+};
+
+const psqlErrorsHandler = (err, req, res, next) => {
+  // TODO: add extra logical errors by codes
+  if (err.code) {
+    console.log("PSQL ERROR: ", err);
+    res.status(400).send({ message: "Bad request" });
+  } else next(err);
+};
+
+const internalErrorsHandler = (err, req, res) => {
+  res.status(500).send({ message: "Internal Server Error" });
+};
+
+module.exports = {
+  notFoundErrorsHandler,
+  customErrorsHandler,
+  psqlErrorsHandler,
+  internalErrorsHandler,
+};
