ddb71b802533c767ff4c6f2143d3f12dcef6c5b5
Nikita Lendiak <lendiak93@gmail.com>
2023-02-23 16:27:55 +0000

    add controllers/models/routers for user details

--- routers/users.router.js
+++ routers/users.router.js
@@ -2,5 +2,6 @@ const usersRouter = require('express').Router();
 const UsersController = require('../controllers/users.controller')
 
 usersRouter.get('/', UsersController.getUsers);
+usersRouter.get('/:username', UsersController.getUserByUsername);
 
 module.exports = usersRouter
