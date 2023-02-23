25279d54982d23cb0bbc1273ad2a2ca8d9eb7913
Nikita Lendiak <lendiak93@gmail.com>
2023-02-23 19:58:35 +0000

    link comments controller method to router

--- routers/comments.router.js
+++ routers/comments.router.js
@@ -3,5 +3,6 @@ const CommentsController = require('../controllers/comments.controller');
 const commentsRouter = require('express').Router();
 
 commentsRouter.delete('/:comment_id', CommentsController.deleteCommentById);
+commentsRouter.patch('/:comment_id', CommentsController.patchComment);
 
 module.exports = commentsRouter;
