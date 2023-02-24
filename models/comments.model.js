0b7342965038060ae030e6b742f2db559f2aa9c8
Nikita Lendiak <lendiak93@gmail.com>
2023-02-24 10:14:12 +0000

    refactor comments model

--- models/comments.model.js
+++ models/comments.model.js
@@ -3,7 +3,8 @@ const CustomError = require("../utils/custom-error");
 
 const selectCommentsByReviewId = async (review_id) => {
   const { rows: comments } = await db.query(
-    `SELECT * FROM comments 
+    `
+     SELECT * FROM comments 
      WHERE review_id = $1 
      ORDER BY created_at DESC;`,
     [review_id]
@@ -12,26 +13,15 @@ const selectCommentsByReviewId = async (review_id) => {
   return comments;
 };
 
-const selectCommentById = async (comment_id) => {
-  const { rows, rowCount } = await db.query(
-    `SELECT * FROM comments 
-     WHERE comment_id = $1;`,
-    [comment_id]
-  );
-
-  if (rowCount === 0)
-    throw new CustomError(404, `Comment with id '${comment_id}' not found`);
-
-  return rows[0];
-};
-
 const insertCommentByReviewId = async (review_id, comment) => {
   const { username: author, body } = comment;
 
   if (body !== undefined && body.length === 0)
     throw new CustomError(400, "Comment cannot be empty");
 
-  const { rows: result } = await db.query(
+  const {
+    rows: [insertedComment],
+  } = await db.query(
     `
     INSERT INTO comments (review_id, author, body)
     VALUES ($1, $2, $3)
@@ -39,7 +29,7 @@ const insertCommentByReviewId = async (review_id, comment) => {
     [review_id, author, body]
   );
 
-  return result[0];
+  return insertedComment;
 };
 
 const updateComment = async (comment_id, patchObject) => {
@@ -49,32 +39,32 @@ const updateComment = async (comment_id, patchObject) => {
     throw new CustomError(400, "Votes only permitted to be changed by 1 or -1");
   }
 
-  const { rows } = await db.query(
+  const {
+    rows: [updatedComment],
+  } = await db.query(
     `
-      UPDATE comments 
-      SET votes = votes + $2
-      WHERE comment_id = $1
-      RETURNING *`,
+    UPDATE comments 
+    SET votes = votes + $2
+    WHERE comment_id = $1
+    RETURNING *`,
     [comment_id, inc_votes]
   );
 
-  return rows[0];
+  return updatedComment;
 };
 
 const deleteCommentFromCommentsById = async (comment_id) => {
-  const { rows } = await db.query(
-    `DELETE FROM comments 
-     WHERE comment_id = $1;`,
+  await db.query(
+    `
+    DELETE FROM comments 
+    WHERE comment_id = $1`,
     [comment_id]
   );
-
-  return rows;
 };
 
 module.exports = {
   selectCommentsByReviewId,
   insertCommentByReviewId,
-  selectCommentById,
   deleteCommentFromCommentsById,
   updateComment,
 };
