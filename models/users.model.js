ef35cd8aed86e1049c36eb71bfbf8b4ee21042d2
Nikita Lendiak <lendiak93@gmail.com>
2023-02-24 10:13:48 +0000

    refactor users model

--- models/users.model.js
+++ models/users.model.js
@@ -3,20 +3,20 @@ const CustomError = require("../utils/custom-error");
 
 const selectUsers = async () => {
   const { rows: users } = await db.query(`SELECT * FROM users`);
-
   return users;
 };
 
 const selectUserByUsername = async (username) => {
-  const { rows, rowCount } = await db.query(
+  const { rows: [user], rowCount } = await db.query(
     `SELECT * FROM users WHERE username = $1`,
     [username]
   );
 
-  if (rowCount === 0)
+  if (rowCount === 0) {
     throw new CustomError(404, `User with username '${username}' not found`);
+  }
 
-  return rows[0];
+  return user;
 };
 
 module.exports = { selectUsers, selectUserByUsername };
