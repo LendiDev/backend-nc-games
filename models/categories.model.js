d8043b358948969ed8fee0b174db9fc16ecd3dd5
Nikita Lendiak <lendiak93@gmail.com>
2023-02-24 10:14:38 +0000

    refactor categories model

--- models/categories.model.js
+++ models/categories.model.js
@@ -1,23 +1,8 @@
 const db = require("../db/connection");
-const CustomError = require("../utils/custom-error");
 
 const selectCategories = async () => {
-  const { rows, rowCount } = await db.query(`SELECT * FROM categories;`);
-
-  if (rowCount === 0) throw new CustomError(404, "No categories found");
-
-  return rows;
+  const { rows: categories } = await db.query(`SELECT * FROM categories;`);
+  return categories;
 };
 
-const selectCategoryBySlug = async (category_slug) => {
-  const { rows, rowCount } = await db.query(
-    `SELECT * FROM categories WHERE slug = $1`,
-    [category_slug]
-  );
-
-  if (rowCount === 0) throw new CustomError(404, "Category not found");
-
-  return rows[0];
-};
-
-module.exports = { selectCategories, selectCategoryBySlug };
+module.exports = { selectCategories };
